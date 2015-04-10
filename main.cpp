7d4bff18c728d9024518266faa797d8d10f266f9
Sam Graham <samgraham@outlook.com>
2015-04-09 21:01:12 -0600

    Added in empty main class

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*************************************************************
+ * Main driver for the AKS class 
+ *
+ * For:
+ * AKS Primality Test
+ *
+ * By Sam Graham and Tyson Graham
+ * At BYU-Idaho
+ * 04/09/2015
+ ************************************************************/
+#include <iostream>
+#include <gmp>
+
+using namespace std;
+
+/***********************************************************
+ * This is the main class
+ ***********************************************************/
+int main(void)
+{
+
+  return 0;
+}

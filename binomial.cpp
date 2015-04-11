cda01e7f408c72153c54155c62fa2734f4321de4
Sam Graham <samgraham@outlook.com>
2015-04-11 00:37:54 -0600

    Comment modification

--- binomial.cpp
+++ binomial.cpp
@@ -6,12 +6,14 @@
  * This uses the AKS prime finding method using the gmp
  * library
  ****************************************************************/
+
 #include <gmp.h>
 #include "binomial.h"
+
 /***************************************************************
-* This function casts an mpz_t to an unsigned long long. We will
-* possibly need this function in order to return the mpz_t value
-* as we don't know how to pass the value of the mpz_t function.
+* This function casts an mpz_t to an unsigned long long. We
+* need this function in order to return the mpz_t value due to 
+* the difficulty of passing the value of the mpz_t function.
 ***************************************************************/
 unsigned long long mpz_get_ull(mpz_t n)
 {
@@ -31,11 +33,19 @@ unsigned long long mpz_get_ull(mpz_t n)
 }
 
 /************************************************************
-* There is a binomial function for other data types. However,
-* there doesn't seem to be a binomial function for mpz_t values.
-* For this reason, we are trying to adapt Brother Neff's 
-* int binomial function and replace it with an unsigned
-* long long so that we can get the value for larger numbers.
+* We used the GMP library's unsigned long long binomial
+* coefficient function whose data will be used in the aks.cpp
+* file. This is where most of the program's optimizations will
+* be needed as we could use dynamic programming for this 
+* portion of our program's tasks. This would greatly speed
+* up our program and is really the bottleneck that makes
+* our program take so long for large numbers. However, we 
+* must marvel at the fact that while it takes a long time,
+* our program can handle transcendentally large numbers and
+* determine whether they are prime or composite in  puesdo-
+* polynomial time! The dynamic programming optimization will
+* have to wait for another day! But we hope to make time
+* for it in the near future!
 *************************************************************/
 void binomial (mpz_t retVal, mpz_t topNum, mpz_t bottomNum)
 {
